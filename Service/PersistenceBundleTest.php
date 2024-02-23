b0bed2230d5a8376e8b17403ec6bde70c29399fe
Filip Horvat <fico7489@gmail.com>
2024-02-23 08:48:19 +0100

    init

---
+++ Service/PersistenceBundleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SymfonyEs\Bundle\PersistenceBundle\Service;
+
+class PersistenceBundleTest
+{
+    public function test()
+    {
+        return 'PersistenceBundleTest->test1234....';
+    }
+}
