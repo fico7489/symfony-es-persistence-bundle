9ffee85ab2c4d84353c1d036b00218404eb01e63
Filip Horvat <fico7489@gmail.com>
2024-02-23 09:45:05 +0100

    update

--- DependencyInjection/PersistenceExtension.php
+++ DependencyInjection/PersistenceExtension.php
@@ -7,9 +7,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class ERPEsCacheExtension extends Extension
+class PersistenceExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container):
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/'));
 
